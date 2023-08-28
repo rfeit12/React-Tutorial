044612f4fbc1274ea69b6cb558afbd06c4750dcf
renan-feitosa <renan.oliveira@sou.inteli.edu.br>
2023-08-28 10:34:24 -0300

    create react

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá!</h1>
+      <p>Teste react</p>
+    </div>
+  );
+}
+
+export default App;
