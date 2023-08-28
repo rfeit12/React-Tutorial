fc57b5d43f6245d9fb5ac2963f5adfe5b578a6cb
renan-feitosa <renanfeit12@gmail.com>
2023-08-28 11:04:11 -0300

    Add componentes

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
   return (
     <div className="App">
       <h1>Olá!</h1>
       <p>Teste react</p>
+      <p>Soma: {sum(1,2)}</p>
+      <img src={url} alt="Minha imagem" />
+      <HelloWorld />
     </div>
   );
 }
