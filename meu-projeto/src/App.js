2b7cfa39d7a670f54ea464927656765e2edc5052
renan-feitosa <renanfeit12@gmail.com>
2023-08-29 11:31:17 -0300

    Add Fragments

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -4,6 +4,7 @@ import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa'
 import Frase from './components/Frase';
+import List from './components/List';
 
 function App() {
   const nome = 'Maria';
@@ -19,6 +20,7 @@ function App() {
         profissao='Programador'
         foto='https://via.placeholder.com/150'
       />
+      <List />
     </div>
   );
 }
